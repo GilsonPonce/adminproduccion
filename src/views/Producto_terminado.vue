4c2284c1a142a6c3443a41284b5ba884349a95a7
novared.com.ec <sistema@novared.com.ec>
2021-05-12 12:58:38 -0500

    Funcionalidad de producto terminado

--- src/views/Producto_terminado.vue
+++ src/views/Producto_terminado.vue
@@ -1,16 +1,26 @@
 <template>
+<div v-if="activarFormulario">
+    <div class="contenedor">
+      <div class="modelo">
+        <div>
+          <formProductoTerminado />
+        </div>
+      </div>
+    </div>
+  </div>
 <tableShow nombreTabla="Producto terminado" pagina="producto_terminado" />
 </template>
 
 <script>
 import tableShow from "@/components/tableShow.vue";
-import { onUpdated,onMounted, ref } from 'vue';
+import formProductoTerminado from "../components/formProductoTerminado.vue";
+import { onUpdated,onMounted,computed, ref } from 'vue';
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex';
 export default {
 name: "ProductoTerminado",
   components: {
-    tableShow
+    tableShow, formProductoTerminado
   },
 setup(){
 
@@ -31,11 +41,35 @@ setup(){
     }
     filtroProductoTerminado();
 
-    return{filtroProductoTerminado}
+     const activarFormulario = computed(() => {
+      return store.state.formulario;
+    });
+
+
+    return{filtroProductoTerminado, activarFormulario}
 }
 }
 </script>
 
 <style scoped>
-
+.contenedor {
+  position: fixed;
+  z-index: 3;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.541);
+}
+.modelo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  border-radius: 10px;
+  padding: 20px;
+  margin: auto 200px;
+  position: relative;
+  top: 18%;
+}
 </style>
